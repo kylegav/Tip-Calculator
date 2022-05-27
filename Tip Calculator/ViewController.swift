ff1caf4a5904a089ee91c34ea318abbb9161ca0c
kylegav <60828879+kylegav@users.noreply.github.com>
2022-05-27 17:45:20 -0400

    Added readme

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Object Variables for Storyboard
+    
+    
+    
+    @IBOutlet weak var displayText: UITextField!
+
+    @IBOutlet weak var varTip: UILabel!
+    
+    @IBOutlet weak var tipPercent: UISegmentedControl!
+    
+    @IBOutlet weak var varTotal: UILabel!
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calcTip(_ sender: Any) {
+        
+        let bill = Double(displayText.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipPercent.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        varTip.text = String(format: "$%.2f", tip)
+        
+        varTotal.text = String(format: "$%.2f", total)
+    }
+    
+    
 }
 
